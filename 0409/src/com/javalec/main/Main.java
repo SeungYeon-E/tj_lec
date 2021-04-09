e44d022c92c43bdcf875b08dabe3a4684b221927
gr2nsky <44865986+gr2nsky@users.noreply.github.com>
2021-04-09 18:42:15 +0900

    greensky
    today lec

--- 0409/src/com/javalec/main/Main.java
+++ 0409/src/com/javalec/main/Main.java
@@ -3,6 +3,7 @@ package com.javalec.main;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.javalec.collection.base.HashMapLec;
 import com.javalec.collection.base.Student;
 import com.javalec.collection.exercise.ArrayListEx;
 import com.javalec.interfacelec.base.APhone;
@@ -14,13 +15,18 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Running run = new Running();
-		run.coolection_lec02();
+		run.hashMapLec();
 		
 	}
 }
 
 class Running {
 	Scanner sc = new Scanner(System.in);
+	
+	void hashMapLec() {
+		HashMapLec hml = new HashMapLec();
+		hml.lec();
+	}
 	
 	void coolection_lec02() {
 		ArrayList<Student> arr = new ArrayList<Student>();
