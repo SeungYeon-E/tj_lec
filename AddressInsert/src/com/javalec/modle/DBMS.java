827769863755737a1de5bc89ce57fe511ec740b3
gr2nsky <44865986+gr2nsky@users.noreply.github.com>
2021-04-21 23:53:13 +0900

    greensky
    mysql-java crud

--- AddressInsert/src/com/javalec/modle/DBMS.java
+++ AddressInsert/src/com/javalec/modle/DBMS.java
@@ -1,3 +1,8 @@
+/*
+ * 최종작성일자 4.21 / 작성자 윤재필
+ * CRUD 구현 완료
+ */
+
 package com.javalec.modle;
 
 import java.sql.Connection;
@@ -8,14 +13,16 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DBMS {
-	final String url_mysql = "jdbc:mysql://172.30.1.1/useraddress?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	final String ipAddress = "192.168.2.2";
+	final String url_mysql = "jdbc:mysql://" + ipAddress + "/useraddress?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
 	final String id_mysql = "root";
-	final String pw_mysql = "1q2w3e4r!";
+	final String pw_mysql = "1q2w3e4r";
 	
 	Connection conn_mylsq = null;
 	PreparedStatement ps = null;
 	Statement stmt_myslq;
 	
+	//mysql 연결
 	public void mysqlConnect() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -27,7 +34,8 @@ public class DBMS {
 			e.printStackTrace();
 		}
 	}
-	
+
+	//input에 저장된 데이터로 새 튜플을 생성
 	public boolean insertUserInfo(String[] input) {
 		String query = "insert into userinfo (name, telno, address, email, relation) values (?, ?, ?, ?, ?)";
 		
@@ -49,7 +57,7 @@ public class DBMS {
 		}
 	}
 	
-	
+	//테이블의 모든 튜플을 출력
 	public ArrayList<String[]> searchUser() {
 		String query = "select seqno, name, telno, relation from userinfo";
 		ArrayList<String[]> result = new ArrayList<String[]>();
@@ -72,7 +80,7 @@ public class DBMS {
 
 		return result;
 	}
-	
+	//해당 seqNo에 해당하는 튜플을 출력
 	public ArrayList<String> searchOneUser(String seqNo) {
 		System.out.println("[searchOneUser] 시작");
 		String query = "select seqno, name, telno, address, email, relation from userinfo where seqno = " + seqNo ;
@@ -99,4 +107,76 @@ public class DBMS {
 		return result;
 	}
 	
+	//type 속성에서 input 보유한 튜플 출력
+	public ArrayList<String[]> conditionQueryAction(String type, String input) {
+		ArrayList<String[]> rsArr = new ArrayList<String[]>();
+		String query1 = "select seqno, name, telno, relation from userinfo ";
+		String query2 = "where " + type + " like '%" + input + "%'";
+		System.out.println(query1 + "\n" + query2);
+		
+		try {
+			mysqlConnect();
+			ResultSet rs = stmt_myslq.executeQuery(query1 + query2);
+			
+			while(rs.next()) {
+				String[] qTxt = {rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)};
+				rsArr.add(qTxt);
+			}
+			
+			System.out.println("[conditionQueryAction] 불러오기 성공");
+			conn_mylsq.close();
+			System.out.println("[conditionQueryAction] mysql연결 종료");
+		}catch (Exception e) {
+			System.out.println("[conditionQueryAction] 불러오기 실패");
+			e.printStackTrace();
+		}
+		
+		return rsArr;
+	}
+	
+	//해당 seqno에 해당하는 데이터 삭제
+	public boolean deleteAddress(String seqno) {
+		String query = "delete from userinfo where seqno = " + seqno;
+		System.out.println("[deleteAddress] query : " + query);
+		
+		try {
+			mysqlConnect();
+			
+			stmt_myslq.executeUpdate(query);
+			System.out.println("[deleteAddress]삭제 성공");
+			conn_mylsq.close();
+			System.out.println("[deleteAddress] mysql연결 종료");
+			
+			return true;
+		} catch (Exception e) {
+			System.out.println("[deleteAddress]삭제 실패");
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	//seqno의데이터를 수정
+	public boolean updateAddress(String seqno, ArrayList<String> data) {
+		String query1 = "update userinfo ";
+		String query2 = "set name = '" + data.get(1) + "', telno = '" + data.get(2) + 
+				"', address = '" + data.get(3) + "', email = '" + data.get(4) + "', relation = '" + data.get(5) + "' ";
+		String query3 = "where seqno = " + seqno;
+		System.out.println("[deleteAddress] query \n" + query1 + "\n" + query2 + "\n" + query3);
+		
+		try {
+			mysqlConnect();
+			
+			stmt_myslq.executeUpdate(query1 + query2 + query3);
+			System.out.println("[updateAddress] 업데이 성공");
+			conn_mylsq.close();
+			System.out.println("[updateAddress] mysql연결 종료");
+			
+			return true;
+		} catch (Exception e) {
+			System.out.println("[updateAddress] 업데이트 실패");
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
--- AddressInsert/src/com/javalec/view/AddressQuery.java
+++ AddressInsert/src/com/javalec/view/AddressQuery.java
@@ -11,12 +11,14 @@ import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
 import com.javalec.modle.DBMS;
 
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ListSelectionModel;
 import java.awt.event.WindowAdapter;
@@ -24,10 +26,11 @@ import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JRadioButton;
 
 public class AddressQuery {
 
-	DBMS dbms;
+	private DBMS dbms;
 	
 	private JFrame frame;
 	private JTextField tfSearch;
@@ -39,8 +42,13 @@ public class AddressQuery {
 	private JTextField tfEmail;
 	private JTextField tfRelation;
 	private JTextField tfCount;
+	private JComboBox cbSelection;
+	private JButton btnOk;
+	private ButtonGroup radioBtns = new ButtonGroup();
 	
 	ArrayList<JTextField> tfArr;
+	
+	int maxSeqNo = 0;
 	
 	//Table 설정
 	private final DefaultTableModel Outer_Table = new DefaultTableModel();
@@ -87,26 +95,27 @@ public class AddressQuery {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JComboBox cbSelection = new JComboBox();
-		cbSelection.setModel(new DefaultComboBoxModel(new String[] {"이름", "전화번호", "이메일", "주소", "관계"}));
-		cbSelection.setBounds(27, 24, 85, 27);
+		cbSelection = new JComboBox();
+		cbSelection.setModel(new DefaultComboBoxModel(new String[] {"이름", "주소", "관계"}));
+		cbSelection.setBounds(27, 46, 85, 27);
 		frame.getContentPane().add(cbSelection);
 		
 		tfSearch = new JTextField();
-		tfSearch.setBounds(124, 23, 247, 26);
+		tfSearch.setBounds(110, 45, 238, 26);
 		frame.getContentPane().add(tfSearch);
 		tfSearch.setColumns(10);
 		
 		JButton btnSeach = new JButton("검색");
 		btnSeach.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				conditionQuery();
 			}
 		});
-		btnSeach.setBounds(369, 23, 75, 29);
+		btnSeach.setBounds(345, 45, 75, 29);
 		frame.getContentPane().add(btnSeach);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(37, 63, 382, 106);
+		scrollPane.setBounds(37, 82, 382, 106);
 		frame.getContentPane().add(scrollPane);
 		
 		Inner_Table = new JTable();
@@ -138,7 +147,7 @@ public class AddressQuery {
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		tfTelNo = new JTextField();
-		tfTelNo.setEditable(false);
+		tfTelNo.setEnabled(false);
 		tfTelNo.setColumns(10);
 		tfTelNo.setBounds(91, 256, 130, 26);
 		frame.getContentPane().add(tfTelNo);
@@ -149,7 +158,7 @@ public class AddressQuery {
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		tfAddress = new JTextField();
-		tfAddress.setEditable(false);
+		tfAddress.setEnabled(false);
 		tfAddress.setColumns(10);
 		tfAddress.setBounds(91, 316, 280, 26);
 		frame.getContentPane().add(tfAddress);
@@ -160,7 +169,7 @@ public class AddressQuery {
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		tfName = new JTextField();
-		tfName.setEditable(false);
+		tfName.setEnabled(false);
 		tfName.setColumns(10);
 		tfName.setBounds(93, 223, 130, 26);
 		frame.getContentPane().add(tfName);
@@ -171,7 +180,7 @@ public class AddressQuery {
 		frame.getContentPane().add(lblNewLabel_4);
 		
 		tfEmail = new JTextField();
-		tfEmail.setEditable(false);
+		tfEmail.setEnabled(false);
 		tfEmail.setColumns(10);
 		tfEmail.setBounds(91, 288, 130, 26);
 		frame.getContentPane().add(tfEmail);
@@ -182,7 +191,7 @@ public class AddressQuery {
 		frame.getContentPane().add(lblNewLabel_2_1);
 		
 		tfRelation = new JTextField();
-		tfRelation.setEditable(false);
+		tfRelation.setEnabled(false);
 		tfRelation.setColumns(10);
 		tfRelation.setBounds(91, 344, 130, 26);
 		frame.getContentPane().add(tfRelation);
@@ -191,12 +200,12 @@ public class AddressQuery {
 		tfCount.setEditable(false);
 		tfCount.setHorizontalAlignment(SwingConstants.TRAILING);
 		tfCount.setColumns(10);
-		tfCount.setBounds(354, 176, 51, 26);
+		tfCount.setBounds(345, 190, 51, 26);
 		frame.getContentPane().add(tfCount);
 		
 		JLabel lblNewLabel_5 = new JLabel("명");
 		lblNewLabel_5.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_5.setBounds(395, 181, 24, 16);
+		lblNewLabel_5.setBounds(386, 195, 24, 16);
 		frame.getContentPane().add(lblNewLabel_5);
 		
 		tfArr.add(tfSeqNo);
@@ -205,6 +214,40 @@ public class AddressQuery {
 		tfArr.add(tfAddress);
 		tfArr.add(tfEmail);
 		tfArr.add(tfRelation);
+		
+		JRadioButton rdbtnInsert = new JRadioButton("입력");
+		rdbtnInsert.addActionListener(new radioCliecked());
+		rdbtnInsert.setBounds(38, 11, 61, 23);
+		frame.getContentPane().add(rdbtnInsert);
+		radioBtns.add(rdbtnInsert);
+		
+		JRadioButton rdbtnUpdate = new JRadioButton("수정");
+		rdbtnUpdate.addActionListener(new radioCliecked());
+		rdbtnUpdate.setBounds(107, 11, 61, 23);
+		frame.getContentPane().add(rdbtnUpdate);
+		radioBtns.add(rdbtnUpdate);
+		
+		JRadioButton rdbtnDelete = new JRadioButton("삭제");
+		rdbtnDelete.addActionListener(new radioCliecked());
+		rdbtnDelete.setBounds(180, 10, 61, 23);
+		frame.getContentPane().add(rdbtnDelete);
+		radioBtns.add(rdbtnDelete);
+		
+		JRadioButton rdbtnSearch = new JRadioButton("검색");
+		rdbtnSearch.setSelected(true);
+		rdbtnSearch.addActionListener(new radioCliecked());
+		rdbtnSearch.setBounds(263, 11, 61, 23);
+		frame.getContentPane().add(rdbtnSearch);
+		radioBtns.add(rdbtnSearch);
+		
+		btnOk = new JButton("OK");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				classification();
+			}
+		});
+		btnOk.setBounds(313, 391, 117, 29);
+		frame.getContentPane().add(btnOk);
 	}
 	
 	private void tableInit() {
@@ -237,15 +280,21 @@ public class AddressQuery {
 		col = Inner_Table.getColumnModel().getColumn(3);
 		col.setPreferredWidth(200);
 	}
-	
+	//최초 모든 데이터를 테이블에 출력
 	private void searchAction(){
 		ArrayList<String[]> rsArr = dbms.searchUser();
 		for (int i = 0; i <= rsArr.size() - 1; i++) {
 			Outer_Table.addRow(rsArr.get(i));
 		}
 		tfCount.setText(Integer.toString(rsArr.size()));
+		
+		//rsArr.size()번째 로우의 seqno를 가져와 maxSeqNo에 입력해야 함
+		String wkSeq = (String) Inner_Table.getValueAt(rsArr.size() - 1, 0);
+		maxSeqNo = Integer.parseInt(wkSeq);
+		System.out.println("[searchAction]maxSeqNo = " + maxSeqNo);
 	}
 	
+	//테이플의 아이템을 클릭시 하단 tf에 상세내용 출력
 	private void tableClick() {
 		ArrayList<String> rsArr = new ArrayList<String>();
 		int row = Inner_Table.getSelectedRow();
@@ -260,14 +309,208 @@ public class AddressQuery {
 		}
 	}
 	
+	//입력
+	private void insertAction() {
+		String[] inputArr = new String[5];
+		for (int i = 1; i <= 5; i++) {
+			JTextField tf = tfArr.get(i);
+			String input = tf.getText().trim();
+			if (input.equals("")) {
+				JOptionPane.showMessageDialog(null, "항목을 모두 작성해 주세요.");
+				tf.requestFocus();
+				return;
+			}
+			inputArr[i - 1] = input;
+		}
+		System.out.println("[insertAction] 수행완료");
+		if (dbms.insertUserInfo(inputArr)) {
+			JOptionPane.showMessageDialog(null, "입력 성공");
+			Outer_Table.addRow(getTableDataFromTFArray(1));
+			int newSeq = Integer.parseInt(tfCount.getText().toString()) + 1;
+			tfCount.setText(Integer.toString(newSeq));
+			clearColumn();
+		} else {
+			JOptionPane.showMessageDialog(null, "입력실패");
+		}
+	}
+	
+	//조건검색
+	private void conditionQuery() {
+		ArrayList<String[]> rsArr = new ArrayList<String[]>();
+		int type = cbSelection.getSelectedIndex();
+		String conditionQueryColumn = "";
+		String search = tfSearch.getText();
+		
+		switch (type) {
+		case 0:
+			conditionQueryColumn = "name";
+			break;
+		case 1:
+			conditionQueryColumn = "address";
+			break;
+		case 2:
+			conditionQueryColumn = "relation";
+			break;
+		default:
+			break;
+		}
+		System.out.println(conditionQueryColumn);
+		tableInit();
+		clearColumn();
+		rsArr = dbms.conditionQueryAction(conditionQueryColumn, search);
+		
+		for (int i = 0; i <= rsArr.size() - 1; i++) {
+			Outer_Table.addRow(rsArr.get(i));
+		}
+		tfCount.setText(Integer.toString(rsArr.size()));
+	}
 	
 	
+	//하단 tf 클리어
+	private void clearColumn() {
+		for (int i = 0; i < tfArr.size(); i++) {
+			tfArr.get(i).setText("");
+		}
+	}
 	
+	//해당 컬럼을 삭제하고, 카운트 1감소
+	private void deleteColumn(int row) {
+		int count = Integer.parseInt(tfCount.getText());
+		Outer_Table.removeRow(row);
+		tfCount.setText(Integer.toString(count - 1));
+	}
 	
+	//수정된 칼럼을 기본 컬럼 앞에 추가
+	private void updateColumn(int row, String[] updateDatas) {
+		Outer_Table.insertRow(row, updateDatas);
+	}
 	
+	//정보 삭제
+	private void deleteAddress() {
+		//선택된 행번호 추출
+		int row = Inner_Table.getSelectedRow();
+		//선택하지 않았을 경우 예외처리
+		if (row == -1) {
+			JOptionPane.showMessageDialog(null, "선택된 항목이 없습니다.");
+			return;
+		}
+		String wkSeq = (String) Inner_Table.getValueAt(row, 0);
+		System.out.println("선택된 열 : " + row + " // seq번호 :" + wkSeq);
+
+		if (dbms.deleteAddress(wkSeq)) {
+			JOptionPane.showMessageDialog(null, "삭제 성공");
+			deleteColumn(row);
+			clearColumn();
+		} else {
+			JOptionPane.showMessageDialog(null, "삭제 실패");
+		}
+	}
 	
+	//정보 수정
+	private void updateAddress() {
+		ArrayList<String> updateData = new ArrayList<String>();
+		//선택된 행번호 추출
+		int row = Inner_Table.getSelectedRow();
+		//선택하지 않았을 경우 예외처리
+		if (row == -1) {
+			JOptionPane.showMessageDialog(null, "선택된 항목이 없습니다.");
+			return;
+		}
+		//선택된테이블 아이템의 seqno 추출
+		String wkSeq = (String) Inner_Table.getValueAt(row, 0);
+		System.out.println("선택된 열 : " + row + " // seq번호 :" + wkSeq);
+		
+		for(JTextField tf : tfArr) {
+			updateData.add(tf.getText());
+		}
+		if (dbms.updateAddress(wkSeq, updateData)) {
+			JOptionPane.showMessageDialog(null, "수정 성공");
+			//해당 row에 아이템이 추가되므로, row+1번째 행을 삭제해야 기존 데이터를 지울 수 있다.
+			updateColumn(row, getTableDataFromTFArray(0));
+			deleteColumn(row + 1);
+			clearColumn();
+		} else {
+			JOptionPane.showMessageDialog(null, "수정 실패");
+		}
+	}
 	
+	//tf의 arraylist에서 테이블에 입력할 값만 추출해 배열로 반환
+	//type 0이 디폴트, 1은 입력
+	private String[] getTableDataFromTFArray(int type) {
+		if (type == 0) {
+			String[] data = {tfArr.get(0).getText(), tfArr.get(1).getText(), tfArr.get(2).getText(), tfArr.get(5).getText()};
+			return data;
+		} else {
+			String[] data = {Integer.toString(maxSeqNo + 1), tfArr.get(1).getText(), tfArr.get(2).getText(), tfArr.get(5).getText()};
+			System.out.println("[getTableDataFromTFArray] 입력값 : " + data);
+			return data;
+		}
+
+	}
 	
+	private void classification() {
+		switch (btnOk.getText().toString()) {
+		case "추가":
+			insertAction();
+			break;
+		case "수정":
+			updateAddress();
+			break;
+		case "삭제":
+			deleteAddress(); 
+			break;
+		default:
+			break;
+		}
+		System.out.println("clicked btn is " + btnOk.getText().toString());
+	}
 	
-	
+	class radioCliecked implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			switch (e.getActionCommand()) {
+			case "입력":
+				clearColumn();
+				tfSetOn();
+				Inner_Table.setEnabled(false);
+				btnOk.setText("추가");
+				btnOk.setVisible(true);
+				break;
+			case "수정":
+				tfSetOn();
+				Inner_Table.setEnabled(true);
+				btnOk.setText("수정");
+				btnOk.setVisible(true);
+				break;
+			case "삭제":
+				tfSetOff();
+				Inner_Table.setEnabled(true);
+				btnOk.setText("삭제");
+				btnOk.setVisible(true);
+				break;
+			case "검색":
+				tfSetOff();
+				Inner_Table.setEnabled(true);
+				btnOk.setVisible(false);
+				break;
+
+			default:
+				break;
+			}
+			
+		}
+		
+		void tfSetOff() {
+			for (int i = 1; i < tfArr.size(); i++) {
+				tfArr.get(i).setEnabled(false);
+			}
+		}
+		
+		void tfSetOn() {
+			for (int i = 1; i < tfArr.size(); i++) {
+				tfArr.get(i).setEnabled(true);
+			}
+		}
+	}
+
 }
